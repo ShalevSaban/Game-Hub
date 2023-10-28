142bb3fd54f0ff2c26e3a41b46f2efa51e8d3946
ShalevSaban <shalevsaban1@gmail.com>
2023-10-28 15:59:01 +0300

    Fix game grid

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "3d1e8c63e81c4c2d9b69e7a775b7a78a",
+    key: "a0ee81abac8b4b239ef33a728361d3f3",
   },
 });
